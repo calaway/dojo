7a978360085d6c79e9dd0188ee55fb0edae94e11
Calaway <calaway@users.noreply.github.com>
2017-02-16 10:00:45 -0700

    Exercism - Ruby - Hamming - Tests passing: 13/16.

--- exercism/ruby/hamming/hamming.rb
+++ exercism/ruby/hamming/hamming.rb
@@ -1,5 +1,9 @@
 class Hamming
   def self.compute(original, mutation)
-    0
+    zipped_chars = original.chars.zip(mutation.chars)
+
+    zipped_chars.count do |zipped_char|
+      zipped_char.first != zipped_char.last
+    end
   end
 end
