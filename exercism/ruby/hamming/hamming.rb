ab9ee19b8fc654b87fcd0d4d3590c6f91170464e
Calaway <calaway@users.noreply.github.com>
2017-02-17 13:06:06 -0700

    Exercism - Ruby - Hamming - Tests passing: 16/16.

--- exercism/ruby/hamming/hamming.rb
+++ exercism/ruby/hamming/hamming.rb
@@ -1,9 +1,18 @@
 class Hamming
   def self.compute(original, mutation)
-    zipped_chars = original.chars.zip(mutation.chars)
+    validate_arguments(original, mutation)
 
+    zipped_chars = original.chars.zip(mutation.chars)
     zipped_chars.count do |zipped_char|
       zipped_char.first != zipped_char.last
     end
   end
+
+  def self.validate_arguments(original, mutation)
+    raise ArgumentError if original.length != mutation.length
+  end
+end
+
+module BookKeeping
+  VERSION = 3
 end
