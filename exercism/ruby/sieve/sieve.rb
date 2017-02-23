ad1ff83ceefe31c1e3655d99fae60b6f151c1980
Calaway <calaway@users.noreply.github.com>
2017-02-23 00:23:48 -0700

    Exercism - Ruby - Sieve - Tests passing: 5/6.

--- exercism/ruby/sieve/sieve.rb
+++ exercism/ruby/sieve/sieve.rb
@@ -1,11 +1,16 @@
 class Sieve
-  attr_reader :terminus
-
   def initialize(terminus)
-    @terminus = terminus
+    @numbers = (2..terminus).to_a
   end
 
   def primes
-    (2..terminus).to_a
+    index = 0
+    while index < @numbers.length do
+      @numbers = @numbers[0..index] + @numbers[index+1..-1].delete_if do |number|
+        number % @numbers[index] == 0
+      end
+      index += 1
+    end
+    @numbers
   end
 end
