da46936a3cbeebd84cbea3da08d17ce083edf25a
Calaway <calaway@users.noreply.github.com>
2017-02-27 15:00:42 -0700

    Exercism - Ruby - Nth Prime - Tests passing: 4/6

--- exercism/ruby/nth-prime/nth_prime.rb
+++ exercism/ruby/nth-prime/nth_prime.rb
@@ -2,10 +2,22 @@ class Prime
   def self.nth(n)
     prime = 2
     count = 1
+    trial_prime = 3
     while count != n do
-      prime = prime + 1
-      count += 1
+      if is_prime?(trial_prime)
+        prime = trial_prime
+        count += 1
+      end
+      trial_prime += 2
     end
     prime
+  end
+
+  def self.is_prime?(trial_prime)
+    max_trial_divisor = Math.sqrt(trial_prime).floor
+    all_trial_divisors = (3..max_trial_divisor).step(2).to_a
+    all_trial_divisors.none? do |trial_divisor|
+      trial_prime % trial_divisor == 0
+    end
   end
 end
