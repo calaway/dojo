4c2c397dbdda9d36db453b69240798437039f422
Calaway <calaway@users.noreply.github.com>
2018-01-14 11:41:51 -0700

    Complete word_count exercism

--- exercism/ruby/word-count/word_count.rb
+++ exercism/ruby/word-count/word_count.rb
@@ -1,7 +1,20 @@
 class Phrase
-  def initialize(phrase); end
+  attr_reader :phrase
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
 
   def word_count
-    { 'word' => 1 }
+    phrase_without_quotes = phrase.gsub(/'\w+'/) do |match|
+      match[1..-2]
+    end
+    phrase_without_quotes.split(/[^\w\d']+/).each_with_object(Hash.new(0)) do |word, counts|
+      counts[word.downcase] += 1
+    end
   end
+end
+
+module BookKeeping
+  VERSION = 1
 end
