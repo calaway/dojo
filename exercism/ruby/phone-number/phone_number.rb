39bbb01bfb4b0f7761efe54e1726b4bb59877abf
Calaway <calaway@users.noreply.github.com>
2018-01-24 09:38:08 -0700

    Refactor to make single purpose methods

--- exercism/ruby/phone-number/phone_number.rb
+++ exercism/ruby/phone-number/phone_number.rb
@@ -1,11 +1,32 @@
 class PhoneNumber
   def self.clean(phone_number)
-    phone_number = phone_number.gsub(/[^0-9]/, '')
-    phone_number = phone_number[1..-1] if phone_number.length > 10 && phone_number[0] == '1'
-    return if phone_number.length != 10
-    return if ('0'..'1').cover?(phone_number[0])
-    return if ('0'..'1').cover?(phone_number[3])
-    '2234567890'
+    phone_number = clean_non_digits(phone_number)
+    phone_number = clean_country_code(phone_number)
+    return if invalid_length?(phone_number)
+    return if invalid_area_code?(phone_number)
+    return if invalid_exchange_code?(phone_number)
+    phone_number
+  end
+
+  def self.clean_non_digits(phone_number)
+    phone_number.gsub(/[^0-9]/, '')
+  end
+
+  def self.clean_country_code(phone_number)
+    return phone_number[1..-1] if phone_number[0] == '1'
+    phone_number
+  end
+
+  def self.invalid_length?(phone_number)
+    phone_number.length != 10
+  end
+
+  def self.invalid_area_code?(phone_number)
+    ('0'..'1').cover?(phone_number[0])
+  end
+
+  def self.invalid_exchange_code?(phone_number)
+    ('0'..'1').cover?(phone_number[3])
   end
 end
 
