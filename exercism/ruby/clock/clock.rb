a3668a832360b81c248b52c29483561fac7c1802
Calaway <calaway@users.noreply.github.com>
2018-01-29 11:28:58 -0700

    Refactor to create and implement fix_minutes_and_hours method

--- exercism/ruby/clock/clock.rb
+++ exercism/ruby/clock/clock.rb
@@ -2,20 +2,25 @@ class Clock
   def initialize(hour, minute)
     @hour = hour
     @minute = minute
+    fix_minutes_and_hours
   end
 
   def self.at(hour, minute)
     new(hour, minute)
   end
 
-  def to_s
+  def fix_minutes_and_hours
     @hour = (@hour + (@minute / 60)) % 24
     @minute %= 60
+  end
+
+  def to_s
     "#{@hour.to_s.rjust(2, '0')}:#{@minute.to_s.rjust(2, '0')}"
   end
 
   def +(other)
     @minute += other
+    fix_minutes_and_hours
     self
   end
 end
