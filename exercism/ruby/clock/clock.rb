1db77843d70e9863d1e605e57cd5eb6d435450ce
Calaway <calaway@users.noreply.github.com>
2018-01-29 09:57:01 -0700

    Refactor to create instance of clock class and add 'to_s' instance method

--- exercism/ruby/clock/clock.rb
+++ exercism/ruby/clock/clock.rb
@@ -1,8 +1,23 @@
-module Clock
+class Clock
+  def initialize(hour_input, minute_input)
+    @hour_input = hour_input
+    @minute_input = minute_input
+  end
+
   def self.at(hour, minute)
-    hour += minute / 60
-    hour = hour % 24
-    minute = minute % 60
+    new(hour, minute)
+  end
+
+  def hour
+    hour = @hour_input + (@minute_input / 60)
+    hour % 24
+  end
+
+  def minute
+    @minute_input % 60
+  end
+
+  def to_s
     "#{hour.to_s.rjust(2, '0')}:#{minute.to_s.rjust(2, '0')}"
   end
 end
