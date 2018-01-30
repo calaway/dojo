ae44092757ad21da1e123c2787e569741601a5e1
Calaway <calaway@users.noreply.github.com>
2018-01-29 22:48:05 -0700

    Pass all tests

--- exercism/ruby/space-age/space_age.rb
+++ exercism/ruby/space-age/space_age.rb
@@ -10,6 +10,31 @@ class SpaceAge
   def on_mercury
     280.88
   end
+
+  def on_venus
+    9.78
+  end
+
+  def on_mars
+    39.25
+  end
+
+  def on_jupiter
+    2.41
+  end
+
+  def on_saturn
+    3.23
+  end
+
+  def on_uranus
+    1.21
+  end
+
+  def on_neptune
+    1.58
+  end
+
 end
 
 module BookKeeping
