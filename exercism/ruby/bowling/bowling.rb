2fac35a19fddfe35c5b3bca7532e4d78bcaf46ef
Calaway <calaway@users.noreply.github.com>
2018-02-11 22:22:40 -0700

    Pass test #7

--- exercism/ruby/bowling/bowling.rb
+++ exercism/ruby/bowling/bowling.rb
@@ -1,5 +1,33 @@
 class Game
+  attr_reader :score
 
+  def initialize
+    @frame = 0
+    @first_roll = true
+    @previous_pins1 = 0
+    @previous_pins2 = 0
+    @score = 0
+  end
+
+  def roll(pins)
+    @score += if (@frame < 10) && (@previous_pins1 + @previous_pins2 == 10)
+                2 * pins
+              else
+                pins
+              end
+    @previous_pins2 = @previous_pins1
+    @previous_pins1 = pins
+    increment_frame
+  end
+
+  def increment_frame
+    if @first_roll
+      @first_roll = false
+    else
+      @frame += 1
+      @first_roll = true
+    end
+  end
 end
 
 module BookKeeping
