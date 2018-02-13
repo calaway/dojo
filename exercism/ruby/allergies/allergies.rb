b40d9ded80af4edb4039e8bb43a306cd8ed268b3
Calaway <calaway@users.noreply.github.com>
2018-02-12 22:45:47 -0700

    Passing assertions: 5

--- exercism/ruby/allergies/allergies.rb
+++ exercism/ruby/allergies/allergies.rb
@@ -1,8 +1,15 @@
 class Allergies
+  attr_reader :score
+
+  ALLERGENS = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats'].freeze
+
   def initialize(score)
+    @score = score
   end
 
   def allergic_to?(allergen)
+    index = ALLERGENS.index(allergen)
+    score.digits(2)[index] == 1
   end
 end
 
