31901e944c2036ead52dc45cf14872cbf2621b3d
Calaway <calaway@users.noreply.github.com>
2019-06-27 12:07:57 -0600

    Simplify method to find the index of the colors constant instead of using an object of key/value pairs

--- exercism/javascript/resistor-color/resistor-color.js
+++ exercism/javascript/resistor-color/resistor-color.js
@@ -1,17 +1,3 @@
-export const colorCode = (color) => {
-  const colorValues = {
-    'black': 0,
-    'brown': 1,
-    'red': 2,
-    'orange': 3,
-    'yellow': 4,
-    'green': 5,
-    'blue': 6,
-    'violet': 7,
-    'grey': 8,
-    'white': 9,
-  };
-  return colorValues[color]
-};
+export const colorCode = (color) => COLORS.indexOf(color);
 
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
