4f06c555a572c82a7a7bd616085fb80cbce5728a
Calaway <calaway@users.noreply.github.com>
2020-11-05 22:02:27 -0700

    JS space-age passing 2 tests

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,4 +1,17 @@
 export const age = (planet, ageInSeconds) => {
   const secondsPerDay = 31557600;
-  return Math.round((ageInSeconds / secondsPerDay) * 100) / 100;
+  const earthYears = Math.round((ageInSeconds / secondsPerDay) * 100) / 100;
+
+  const orbitalRatio = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  }
+
+  return Math.round((earthYears / orbitalRatio[planet]) * 100) / 100;
 };
